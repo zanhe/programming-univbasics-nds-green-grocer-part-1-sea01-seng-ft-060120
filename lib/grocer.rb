dffb3f08390722959fdd37aaa5cdec47c658eab0
Zana H <zhertica@gmail.com>
2020-06-02 16:17:35 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,7 +1,7 @@
 def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
+  collection.find do |item|
+    item = name
+  end
 
 end
 
